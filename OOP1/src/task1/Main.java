0bd07c1ea85beeb7c6911f834b75de6fc9004cc8
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-06 10:00:34 +0200

    lidt opdateringer

--- OOP1/src/task1/Main.java
+++ OOP1/src/task1/Main.java
@@ -6,10 +6,10 @@ public class Main {
         Driver driver = new Driver("Peter", 19);
         Car car = new Car("BMW", "X5", 2015, "SUV");
         car.setDriver(driver);
-        System.out.println(car + "" + car.getDriver());
+        System.out.println(car + car.getDriver().toString());
 
         Car car2 = new Car("Tesla", "Model S", 2018, "Sedan");
         car2.setDriver(driver);
-        System.out.println(car2 + "" + car2.getDriver());
+        System.out.println(car2 + car2.getDriver().toString());
     }
 }
--- OOP1/src/task2/Main.java
+++ OOP1/src/task2/Main.java
@@ -1,7 +1,6 @@
 package task2;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
